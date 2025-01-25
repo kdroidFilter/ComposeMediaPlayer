d614742bf2a74b92e2db4b23e73d07ca416a783a
Sean Proctor <sproctor@gmail.com>
2025-01-25 17:54:22 -0500

    use toURI instead of constructing our own

--- sample/composeApp/src/androidMain/kotlin/sample/app/App.android.kt
+++ sample/composeApp/src/androidMain/kotlin/sample/app/App.android.kt
@@ -6,6 +6,6 @@ import io.github.vinceglb.filekit.PlatformFile
 actual fun PlatformFile.getUri(): String {
     return when (val androidFile = this.androidFile) {
         is AndroidFile.UriWrapper -> androidFile.uri.toString()
-        is AndroidFile.FileWrapper -> "file://${androidFile.file.absolutePath}"
+        is AndroidFile.FileWrapper -> androidFile.file.toURI().toString()
     }
 }
--- sample/composeApp/src/jvmMain/kotlin/sample/app/App.jvm.kt
+++ sample/composeApp/src/jvmMain/kotlin/sample/app/App.jvm.kt
@@ -1,8 +1,7 @@
 package sample.app
 
 import io.github.vinceglb.filekit.PlatformFile
-import io.github.vinceglb.filekit.path
 
 actual fun PlatformFile.getUri(): String {
-    return "file://$path"
+    return file.toURI().toString()
 }
