c7ad76e3669d75fd65b09976e4e79aa96aba1db3
Elie G <myriam.hadass@gmail.com>
2025-03-30 14:29:21 +0300

    Add play invocation and improve video rendering logic
    Invoke the `play` method after initializing the player state to ensure playback starts automatically. Adjust video rendering in `WindowsVideoPlayerSurface` to use `fillMaxHeight` and explicitly set destination size for better aspect ratio and rendering accuracy.

--- mediaplayer/src/jvmMain/kotlin/io/github/kdroidfilter/composemediaplayer/windows/WindowsVideoPlayerSurface.kt
+++ mediaplayer/src/jvmMain/kotlin/io/github/kdroidfilter/composemediaplayer/windows/WindowsVideoPlayerSurface.kt
@@ -3,11 +3,12 @@ package io.github.kdroidfilter.composemediaplayer.windows
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asComposeImageBitmap
+import androidx.compose.ui.unit.toIntSize
 
 @Composable
 fun WindowsVideoPlayerSurface(
@@ -20,17 +21,19 @@ fun WindowsVideoPlayerSurface(
     ) {
         val bitmap = playerState.currentFrame
         if (bitmap != null) {
-            // Calcul d'un ratio (par exemple 16/9), OU bien vous pouvez calculer ratio = videoWidth / videoHeight
             val ratio = if (playerState.videoWidth != 0 && playerState.videoHeight != 0) {
                 playerState.videoWidth.toFloat() / playerState.videoHeight.toFloat()
             } else 16f / 9f
 
             Canvas(
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .fillMaxHeight()
                     .aspectRatio(ratio)
             ) {
-                drawImage(bitmap.asComposeImageBitmap())
+                drawImage(
+                    bitmap.asComposeImageBitmap(),
+                    dstSize = size.toIntSize()
+                )
             }
         } else {
             // Pas de frame => rien
