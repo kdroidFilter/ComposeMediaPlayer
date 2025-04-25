d81b97843f9cfcebaefe1f9dffce365475fc5abc
Elie Gambache <elyahou.hadass@gmail.com>
2025-04-25 08:04:51 +0300

    Fix bitrate conversion in LinuxVideoPlayerState
    Correctly convert the bitrate from kbps to bps by multiplying by 1000. This ensures accurate metadata representation and prevents potential misinterpretation of bitrate values.

--- mediaplayer/src/jvmMain/kotlin/io/github/kdroidfilter/composemediaplayer/linux/LinuxVideoPlayerState.kt
+++ mediaplayer/src/jvmMain/kotlin/io/github/kdroidfilter/composemediaplayer/linux/LinuxVideoPlayerState.kt
@@ -376,7 +376,8 @@ class LinuxVideoPlayerState : PlatformVideoPlayerState {
                             val bitrate = tagList.getString("bitrate", 0)
                             if (bitrate != null) {
                                 try {
-                                    metadata.bitrate = bitrate.toLong()
+                                    // Convert from kbps to bps by multiplying by 1000
+                                    metadata.bitrate = bitrate.toLong() * 1000
                                 } catch (_: NumberFormatException) {
                                     // Ignore if the string can't be converted to a long
                                 }
